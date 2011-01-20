fc9d4e238e17acab367370d5e1002fd58c9aef67
Christopher Peplin <peplin@bueda.com>
2011-01-20 10:01:48 -0500

    Wrap arguments in log with quotes.

--- src/config.c
+++ src/config.c
@@ -34,12 +34,12 @@ int configure_server(dirt_server* server, char* configuration_path,
             &static_file_path)) {
         strcpy(server->static_file_path, static_file_path);
         log4c_category_log(log4c_category_get("dirt"), LOG4C_PRIORITY_INFO,
-                "Using static file path %s from configuration file",
+                "Using static file path '%s' from configuration file",
                 server->static_file_path);
     } else {
         strcpy(server->static_file_path, DEFAULT_STATIC_FILE_PATH);
         log4c_category_log(log4c_category_get("dirt"), LOG4C_PRIORITY_INFO,
-                "Using default static file path %s", server->static_file_path);
+                "Using default static file path '%s'", server->static_file_path);
     }
  
     config_destroy(configuration);
